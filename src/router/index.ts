dd7ef209d72cb5062fca67fcbd9cd0c10e4ad3f7
Carlos Ferreira <carlos@especializati.com.br>
2024-01-20 10:02:40 -0300

    feat: configure and use vue-router

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomePage from "../pages/HomePage.vue";
+import ContactPage from "../pages/ContactPage.vue";
+
+const routes = [
+    {
+        name: 'home',
+        component: HomePage,
+        path: '/'
+    },
+    {
+        name: 'contact',
+        component: ContactPage,
+        path: '/contato'
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes
+})
+
+export default router
