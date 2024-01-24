078d280b9d8bc67b64db6541e234c1985488d32d
Carlos Ferreira <carlos@especializati.com.br>
2024-01-24 11:39:38 -0300

    feat: show messages errors

--- src/pages/Auth/LoginPage.vue
+++ src/pages/Auth/LoginPage.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import { useUsersStore } from '../../stores/users'
+import router from '../../router'
 
 export default {
     setup() {
@@ -8,20 +9,33 @@ export default {
         const loading = ref(false)
         const email = ref('')
         const password = ref('')
+        const error = reactive({
+            msgError: '',
+            status: 0,
+            active: false
+        })
 
         const auth = () => {
             loading.value = true
             useStore.auth(email.value, password.value)
-            .then(response => alert(response.data.token))
-            .catch(() => alert('ops, falha'))
-            .finally(() => loading.value = false)
+                .then(() => router.push({ name: 'admin.home' }))
+                .catch(responseError => {
+                    error.msgError = 'Falha ao autenticar'
+                    error.status = responseError.response.status
+                    error.active = true
+
+                    if (error.status === 422) error.msgError = 'Dados inválidos'
+                    if (error.status === 404) error.msgError = 'Usuário não encontrado'
+                })
+                .finally(() => loading.value = false)
         }
 
         return {
             email,
             password,
             loading,
-            auth
+            auth,
+            error
         }
     }
 }
@@ -31,9 +45,12 @@ export default {
     <div>
         <h1>Login</h1>
         <form action="#" method="post" @submit.prevent="auth">
+            <div v-if="error.active">
+                <p>{{ error.msgError }}</p>
+            </div>
             <input type="email" name="email" placeholder="E-mail" v-model="email">
             <input type="password" name="password" placeholder="Senha" v-model="password">
-            <button type="submit" :class="{ disabled : loading }">
+            <button type="submit" :class="{ disabled: loading }">
                 <span v-if="loading">Enviando...</span>
                 <span v-else>Login</span>
             </button>
