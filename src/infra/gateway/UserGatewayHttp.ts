3d65efdaadf4d22fbb40ed2270887711a45549f9
Carlos Ferreira <carlos@especializati.com.br>
2024-01-27 10:33:47 -0300

    feat: save token in localStorage

--- src/infra/gateway/UserGatewayHttp.ts
+++ src/infra/gateway/UserGatewayHttp.ts
@@ -1,13 +1,14 @@
-import User from "@/entities/User";
 import httpAdapter from "@/infra/http/HttpClientAdapter";
+import { NAME_TOKEN } from "@/utils/constants";
 
 export default class UserGatewayHttp {
-  async login(email: string, password: string): Promise<User> {
+  async login(email: string, password: string): Promise<any> {
     const device_name = `vue_app${navigator.userAgent}`;
     return await httpAdapter.post("/auth", {
       email,
       password,
       device_name,
-    });
+    })
+    .then((response) => localStorage.setItem(NAME_TOKEN, response.data.token));
   }
 }
