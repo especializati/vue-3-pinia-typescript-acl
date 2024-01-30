3897603cc069ed66f933dcce63cce61cf626f269
Carlos Ferreira <carlos@especializati.com.br>
2024-01-30 10:31:08 -0300

    refactor: improve return promise

--- src/infra/gateway/UserGatewayHttp.ts
+++ src/infra/gateway/UserGatewayHttp.ts
@@ -3,14 +3,17 @@ import httpAdapter from "@/infra/http/HttpClientAdapter";
 import { NAME_TOKEN } from "@/utils/constants";
 
 export default class UserGatewayHttp {
-  async login(email: string, password: string): Promise<any> {
+  async login(email: string, password: string): Promise<Response> {
     const device_name = `vue_app${navigator.userAgent}`;
     return await httpAdapter.post("/auth", {
       email,
       password,
       device_name,
     })
-    .then((response) => localStorage.setItem(NAME_TOKEN, response.data.token));
+    .then((response) => {
+      localStorage.setItem(NAME_TOKEN, response.data.token)
+      return Promise.resolve(response)
+    });
   }
 
   async getMe(): Promise<User> {
--- src/stores/users.ts
+++ src/stores/users.ts
@@ -10,10 +10,10 @@ export const useUsersStore = defineStore('users', {
         users: [] as User[]
     }),
     getters: {
-        hasUsers: (state) => state.users.length > 0
+        hasUsers: (state): boolean => state.users.length > 0
     },
     actions: {
-        async auth(email: string, password: string): Promise<any> {
+        async auth(email: string, password: string): Promise<Response> {
             return await userGateway.login(email, password)
         },
         async getMe(): Promise<void> {
