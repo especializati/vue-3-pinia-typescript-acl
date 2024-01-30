f064ec1484eb90b150b19fb3d23eeb1f56f59a74
Carlos Ferreira <carlos@especializati.com.br>
2024-01-30 10:47:40 -0300

    feat: apply middleware before each

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { NavigationGuardNext, RouteLocationNormalized, createRouter, createWebHistory } from "vue-router";
 import routes from './routers.map'
+import { useUsersStore } from "@/stores/users";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
+router.beforeEach((to: RouteLocationNormalized, _: RouteLocationNormalized, next: NavigationGuardNext) => {
+  const userStore = useUsersStore()
+  const user = userStore.me
+  if (user) return next()
+
+  const routeName = to.name
+  if (routeName !== 'auth.login') {
+    return router.push({ name: 'auth.login' })
+  }
+
+  next()
+})
 
 export default router;
